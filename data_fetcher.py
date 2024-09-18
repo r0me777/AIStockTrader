1842763c2a17d14ffd4f07e5e16bc5935f499753
r0me777 <58448938+r0me777@users.noreply.github.com>
2024-09-18 11:51:07 -0500

    little changes

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def fetch_stock_data(ticker, start_date='2020-01-01', end_date='2023-01-01'):
     stock_data = yf.download(ticker, start=start_date, end=end_date)
-    stock_data = stock_data[['Close']]  # Use only 'Close' price
+    stock_data = stock_data[['Close']]  # Use only 'Close' price -> Will change later to all
     return stock_data
 
